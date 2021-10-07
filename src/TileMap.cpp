5d9085573a36e6649090f42d2a66a9f24abd34d2
Marco Nemetala <marcoantonionemetala@gmail.com>
2021-10-07 11:29:33 -0300

    Começando logica de ler arquivo txt

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -1,5 +1,10 @@
 // Feito por Marco Antonio Nemetala Garcia
 
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
 #include "TileMap.h"
 
 TileMap::TileMap(GameObject& associated, std::string file, TileSet* tileSetado) : Component(associated)
@@ -10,7 +15,30 @@ TileMap::TileMap(GameObject& associated, std::string file, TileSet* tileSetado) 
 
 void TileMap::Load(std::string file)
 {
+    // Abre o arquivo e verifica se foi aberto corretamente;
+    std::ifstream mapeamento(file.c_str());
+    if (!mapeamento)
+    {
+        std::cerr << "Não foi possível abrir o tileMap.txt" << std::endl;
+        std::exit(1);
+    }
 
+    // Pegando as informações do arquivo;
+    std::string cont;
+    std::string dados;
+    std::getline(mapeamento, cont);
+    std::string dimensao = cont;
+    while (std::getline(mapeamento, cont))
+    {
+        if (cont == dimensao)
+        {
+            continue;
+        }
+        dados += cont;
+        dados.push_back('\n');
+    }
+    
+     
 }
 
 void TileMap::SetTileSet(TileSet* tileSet)
